68b3abba2b8208465f05c31056bb7f0267ba338f
efanwu <yifan1030@gmail.com>
2010-12-04 23:08:07 -0600

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Xchange-U"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Xchangeu::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => 'pages#home'
+  
   get "pages/home"
-
   get "pages/contact"
-
   get "pages/about"
+  get "pages/help"
+
+end
+
+ 
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
