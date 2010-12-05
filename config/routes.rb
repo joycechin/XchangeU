e2409e21b2fcd11d51967845304d0fe9913d8b31
efanwu <yifan1030@gmail.com>
2010-12-05 00:23:43 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Xchangeu::Application.routes.draw do
+  get "users/new"
+  
+  match'/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
@@ -70,4 +74,3 @@ end
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
